90738a98c0d6d88fa5a58d89cac67bb7b1a735be
DANIEL LOZANO BORGES P�RES <25254373@educ123.sp.senai.br>
2026-09-02 09:47:03 -0300

    mudando p js e jsx

--- smart-freeze/src/app/page.jsx
+++ smart-freeze/src/app/page.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
 
 export default function Home() {
   return (
@@ -16,7 +16,7 @@ export default function Home() {
         <div className={styles.intro}>
           <h1>
             To get started, edit the{" "}
-            <code className={styles.code}>page.tsx</code> file.
+            <code className={styles.code}>page.js</code> file.
           </h1>
           <p>
             Looking for a starting point or more instructions? Head over to{" "}
