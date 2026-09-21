dcd8d7af750b040dc8036db71c59354e76cbc3dc
DANIEL LOZANO BORGES P�RES <25254373@educ123.sp.senai.br>
2026-09-21 08:41:49 -0300

    stage: desenvolvendo funcionalidades do card

--- smart-freeze/src/app/(aplicacao)/home/page.jsx
+++ smart-freeze/src/app/(aplicacao)/home/page.jsx
@@ -2,47 +2,50 @@ import CardDesktop from "@/components/CardDesktop";
 import styles from "./home.module.scss";
 import Card from "@/components/Card";
 import CardAdd from "@/components/CardAdd";
+import { AppleIcon } from "lucide-react";
+
 
 
 
 export default function Home() {
+
+  const produtos = {
+    nome: "Derick",
+    img: <AppleIcon size={24} />,
+    unidades: 3
+  }
+
+
   return (
-      <main className={styles.main}>
-        <div className={styles.tituloFiltro}>
-          Bem vindo, <span>Gustavo</span>
-        </div>
-        <div className={styles.filtro}>
-          <p className={styles.filtroText}>Filtrar Por:</p>
-          <ul>
-            <li>Filtro 1</li>
-            <li>Filtro 2</li>
-            <li>Filtro 3</li>
-          </ul>
-        </div>
+    <main className={styles.main}>
+      <div className={styles.tituloFiltro}>
+        Bem vindo, <span>Gustavo</span>
+      </div>
+      <div className={styles.filtro}>
+        <p className={styles.filtroText}>Filtrar Por:</p>
+        <ul>
+          <li>Filtro 1</li>
+          <li>Filtro 2</li>
+          <li>Filtro 3</li>
+        </ul>
+      </div>
 
-        <section className={styles.section}>
-          <CardDesktop />
-          <CardDesktop/>
-          <CardDesktop/>
-          <CardDesktop/>
-          <CardDesktop/>
-          <CardDesktop/>
-          <CardDesktop/>
-          <CardDesktop/>
-          <CardDesktop/>
-          <CardDesktop/>
-
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <CardAdd />
-        </section>
-      </main>
+      <section className={styles.section}>
+        <CardDesktop produto={produtos}/>
+        <CardDesktop produto={produtos}/>
+        <CardDesktop produto={produtos}/>
+        <CardDesktop produto={produtos}/>
+        <CardDesktop produto={produtos}/>
+        <CardDesktop produto={produtos}/>
+        
+        <Card produto={produtos} />
+        <Card produto={produtos} />
+        <Card produto={produtos} />
+        <Card produto={produtos} />
+        <Card produto={produtos} />
+        
+        <CardAdd />
+      </section>
+    </main>
   );
 }
--- smart-freeze/src/components/CardDesktop/index.jsx
+++ smart-freeze/src/components/CardDesktop/index.jsx
@@ -1,29 +1,32 @@
 import styles from "./cardDesktop.module.scss";
-import { AppleIcon, PlusIcon, Trash2 } from "lucide-react";
+import { PlusIcon, Trash2 } from "lucide-react";
 
 
-export default function CardDesktop() {
+export default function CardDesktop({produto,Add,Remove}) {
+    if(!produto) return null;
     return (
         <div className={styles.card}>
             <div className={styles.imgBg}>
                 <div className={styles.imgCard}>
-                    <AppleIcon size={0} />
+                    {produto.img}
                 </div>
             </div>
             <div className={styles.textContent}>
                 <div className={styles.cardTitle}>
                     <div className={styles.tituloCard}>
-                        <h3>Título do Card</h3>
+                        <h3>{produto.nome} </h3>
                         <p>
-                            Unidades: <span>3</span>
+                            Unidades: <span>{produto.unidades} </span>
                         </p>
                     </div>
                 </div>
                 <div className={styles.config}>
-                    <button className={styles.addButton}>
+                    <button className={styles.addButton}
+                    onClick={Add}>
                         <PlusIcon size={24} />
                     </button>
-                    <button className={styles.deleteButton}>
+                    <button className={styles.deleteButton}
+                    onClick={Remove}>
                         <Trash2 size={24} />
                     </button>
                 </div>
